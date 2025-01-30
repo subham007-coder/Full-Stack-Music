10eb49dd0a189360c9e3208c7bd445cd495f80a5
subham007-coder <dsubham109@gmail.com>
2025-01-30 14:04:12 +0530

    Refactor App component to improve structure by reorganizing imports and simplifying the component hierarchy

--- Music-Frontend/src/App.jsx
+++ Music-Frontend/src/App.jsx
@@ -1,35 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Header from "./Components/Header";
 import EntryPage from "./Components/EntryPage";
 import Home from "./Components/Home";
 import VerifyOTP from "./Components/VerifyOTP";
 import ChooseArtists from "./Components/ChooseArtists";
 import ChooseSongLanguage from "./Components/ChooseSongLanguage";
 import CreateAccount from "./Components/CreateAccount";
+import Login from "./Components/Login";
+import SettingsPage from './Components/SettingsPage';
 import ProtectedRoute from "./Components/ProtectedRoute";
 import { AudioProvider } from "./context/AudioContext";
 import { UserDataProvider } from "./context/UserDataContext";
-import Login from "./Components/Login";
-import SettingsPage from './Components/SettingsPage';
-import ChooseLanguage from './Components/ChooseSongLanguage';
 
 const App = () => {
   return (
-    <AudioProvider>
-      <UserDataProvider>
-        <Router>
-          {/* <Header /> */}
+    <Router>
+      <AudioProvider>
+        <UserDataProvider>
           <Routes>
             <Route path="/" element={<EntryPage />} />
             <Route path="/create-account" element={<CreateAccount />} />
             <Route path="/login" element={<Login />} />
             <Route path="/verify-otp" element={<VerifyOTP />} />
             <Route path="/choose-artists" element={<ChooseArtists />} />
-            <Route
-              path="/choose-song-language"
-              element={<ChooseSongLanguage />}
-            />
+            <Route path="/choose-song-language" element={<ChooseSongLanguage />} />
             <Route 
               path="/home" 
               element={
@@ -46,18 +40,10 @@ const App = () => {
                 </ProtectedRoute>
               } 
             />
-            <Route 
-              path="/choose-language" 
-              element={
-                <ProtectedRoute>
-                  <ChooseLanguage />
-                </ProtectedRoute>
-              } 
-            />
           </Routes>
-        </Router>
-      </UserDataProvider>
-    </AudioProvider>
+        </UserDataProvider>
+      </AudioProvider>
+    </Router>
   );
 };
 
